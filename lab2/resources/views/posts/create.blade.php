954a1ef48b09e045de00321eb0de7d98955ad7b4
AsmaaHamada3397 <asmaa.amin3397@gmail.com>
2024-08-22 09:58:59 +0300

    create button updated and worked!

--- lab2/resources/views/posts/create.blade.php
+++ lab2/resources/views/posts/create.blade.php
@@ -1,30 +1,24 @@
 @extends('layouts.app')
 
 @section("content")
-    
+
     <div class="container mt-5">
-        <h1>Creat a Post</h1>
-        <form method="post">
+        <h1 class="mt-5 text-danger"> Create your post!</h1>
+        <form method="get">
             <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
                 <input type="text" class="form-control" name="title" id="title">
             </div>
 
-            <div class="input-group mb-3">
-                <input type="file" class="form-control" id="inputGroupFile02">
-                <label class="input-group-text" for="inputGroupFile02">Upload</label>
-            </div>
-
             <div class="mb-3">
                 <label for="description" class="form-label">Description</label>
-                <textarea type="description" class="form-control"  name="description" id="description">
+                <textarea type="description" class="form-control" name="description" id="description"></textarea>
             </div>
-            
-            <div class="mb-3 form-check">
-                <label class="form-check-label" for="exampleCheck1">Post Creator</label>
+            <div class="mb-3">
+                <label for="exampleCheck1">Post Creator</label>
                 <input type="text" class="form-control" id="creator" name="creator">
             </div>
-            <input type="submit" class="btn btn-sucess" value="Create">
+            <button type="submit" class="btn btn-success rounded-3 mt-3">Create</button>
         </form>
     </div>
    
--- lab2/routes/web.php
+++ lab2/routes/web.php
@@ -10,8 +10,8 @@ Route::get('/',
 );
 
 Route::get('/posts', [PostController::class,'index'])->name('posts.index');
+Route::get('/create', [PostController::class, 'create'])->name('posts.create');
 Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
 Route::put('/posts/{id}/edit', [PostController::class, 'edit'])->name('posts.edit');
-Route::get('/posts/create', [postController::class, 'create'])->name('posts.create');
 Route::post('/posts', [PostController::class, 'store'])->name("posts.store");
 Route::get("/post/{id}/delete", [PostController::class, "destroy"]) ->name("posts.destroy");
